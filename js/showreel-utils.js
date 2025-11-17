42b9f05074fbff488b406c9dfe77ed093e2b18aa
motacola <chrisbelgrave@gmail.com>
2025-11-16 22:09:45 -0400

    Fix about animations and showreel filters

---
+++ js/showreel-utils.js
@@ -0,0 +1,13 @@
+import {
+  DEFAULT_FILTER,
+  FILTER_LABELS,
+  getFilterStatusText,
+  resolveFilterState,
+} from "../shared/showreel-utils.js";
+
+export {
+  DEFAULT_FILTER,
+  FILTER_LABELS,
+  getFilterStatusText,
+  resolveFilterState,
+};
